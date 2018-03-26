4f4cf33b82e321212fc9cffdfbef8c2f44a3dcbc
EdricY <yu.edric@gmail.com>
2018-03-26 13:57:07 -0600

    remove .iml and add Hello World

---
+++ src/LabelerMain.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Edric on 3/26/2018.
+ */
+public class LabelerMain {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println(args.length);
+    }
+}
