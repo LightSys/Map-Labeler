107025cc5f03a0bfe9f42994d23e2a6c5ccc85c2
EdricY <yu.edric@gmail.com>
2018-03-30 14:43:02 -0600

    scale by threshold

--- src/Picture.java
+++ src/Picture.java
@@ -307,12 +307,14 @@ public class Picture
         if (Options.targetColor == null){
             if (scorePerPixel < Options.SCORE_THRESHOLD){
                 System.out.println("Warning: " + getFileName() + " might not be labeled well.");
-                Logger.addLog("Warning: Label on " + getFileName() + " is noisy. Consider inspecting output file. Severity: " + Math.round(-scorePerPixel * 100)/100.0);
+                scorePerPixel = scorePerPixel / Options.SCORE_THRESHOLD;
+                Logger.addLog("Warning: Label on " + getFileName() + " is noisy. Consider inspecting output file. Severity: " + Math.round(scorePerPixel * 100)/100.0);
             }
         } else {
             if (scorePerPixel < Options.TC_SCORE_THRESHOLD){
                 System.out.println("Warning: " + getFileName() + " might not be labeled well.");
-                Logger.addLog("Warning: Label on " + getFileName() + " is noisy. Consider inspecting output file. Severity: " + Math.round(-scorePerPixel * 100)/100.0);
+                scorePerPixel = scorePerPixel / Options.TC_SCORE_THRESHOLD;
+                Logger.addLog("Warning: Label on " + getFileName() + " is noisy. Consider inspecting output file. Severity: " + Math.round(scorePerPixel * 100)/100.0);
             }
         }
         Options.font = prevFont;
